a22e88bddc48ff98bfbd17790f8d60cb26b3a6ea
alperennayy <154600553+alperennayy@users.noreply.github.com>
2024-09-21 18:10:48 +0300

    cevre-alan

---
+++ ebob/untitled17.py
@@ -0,0 +1,11 @@
+pi = 3.14
+r = float(input("yarı çap giriniz : "))
+
+cevre = 2*pi*r
+alan = pi*(r**2)
+
+print(f"{r} yarıçaplı dairenizin çevresi : ",cevre)
+print(f"{r} yarıçaplı dairenizin alanı : ",alan)
+
+
+
